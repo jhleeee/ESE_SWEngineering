f6ac1ba7bca52b1ced3574b902b6a50ee9786a40
wonwoo Lee <ljy038@gmail.com>
2014-12-04 20:54:31 +0900

    DB 접속하여 데이터 꺼내오기
    data를 확인하여 꺼내온다.

---
+++ SWEngineering/src/client/gui/DataComfirm.java
@@ -0,0 +1,65 @@
+package test;
+
+import java.io.UnsupportedEncodingException;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.mysql.jdbc.Statement;
+
+public class DataComfirm 
+{
+	 public static void main(String[] args) 
+	 {
+		 java.sql.Connection conn;
+		 Statement stmt = null;
+	     try
+	     {
+	          Class.forName("com.mysql.jdbc.Driver");
+	          conn =  DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/member", "root", "1234"); //결과값을 반드시 받아야 함
+	          System.out.println("DB 연결 완료");
+	          stmt = (Statement)conn.createStatement(); // SQL문 처리용 Statement 객체생성
+	          
+	          ResultSet srs = stmt.executeQuery("select * from guest where id in (' ')");
+	          printData(srs, "id","passoword", "win","lose");
+	          
+	     }
+	     catch(ClassNotFoundException e)
+	     {
+	          System.out.println("JDBC 드라이버 로드 오류");
+	     }
+	     catch(SQLException e)
+	     {
+	          System.out.println("DB 연결 오류");
+	     }
+	     catch(UnsupportedEncodingException e)
+	     { //인코팅 맞는지 확인
+	          System.out.println("지원되지 않는 인코팅 타입");
+	     }
+	 }
+	 private static void printData(ResultSet srs, String col1, String col2, String col3,String col4) throws UnsupportedEncodingException, SQLException
+	 {
+		  while(srs.next())
+		  {
+			  //다음으로 갈수 있으면 true, 아니면 false 
+			   if(!col1.equals(""))
+			   {
+			    System.out.print(new String(srs.getString("id").getBytes("ISO-8859-1")));//한글 코드 변환
+			   }
+			   
+			   if(!col2.equals(""))
+			   {
+			    System.out.print("\t|\t"+new String(srs.getString("password").getBytes("ISO-8859-1")));
+			   }
+			   
+			   if(!col3.equals(""))
+			   {
+			    System.out.print("\t|\t"+new String(srs.getString("win")));
+			   }
+			   if(!col3.equals(""))
+			   {
+				    System.out.println("\t|\t"+new String(srs.getString("lose")));
+			   }
+		  }
+	}		 
+}
