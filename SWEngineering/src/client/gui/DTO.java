76006d7f9bb031a62b90734d3e87e87d9de57f2f
wonwoo Lee <ljy038@gmail.com>
2014-12-05 11:21:30 +0900

    로그인, DB 소스 재업로드

---
+++ SWEngineering/src/client/gui/DTO.java
@@ -0,0 +1,43 @@
+package test;
+ 
+public class DTO 
+{
+	String id;
+	String password;
+	String win;
+	String lose;
+
+	public String getId() 
+	{
+		return id;
+	}
+	public void setId(String id) 
+	{
+		this.id = id;
+	}
+	public String getPassword() 
+	{
+		return password;
+	}
+	public void setPassword(String password) 
+	{
+		this.password = password;
+	}
+	
+	public String getWin()
+	{
+		return win;
+	}
+	public void setWin(String win)
+	{
+		this.win = win;
+	}
+	public String getLose()
+	{
+		return lose;
+	}
+	public void setLose(String lose)
+	{
+		this.lose = lose;
+	}
+}
