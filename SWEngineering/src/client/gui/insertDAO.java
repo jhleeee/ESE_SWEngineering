5216c153b71a948f75c7f88272ad11c32400c828
wonwoo Lee <ljy038@gmail.com>
2014-12-05 11:16:08 +0900

    로그인, DB 소스

--- SWEngineering/src/client/gui/insertDAO.java
+++ SWEngineering/src/client/gui/insertDAO.java
@@ -16,18 +16,18 @@ public class insertDAO
 	   Connection con = null;
 	   Statement stmt = null; // 데이터를 전송하는 객체
 	   String id = dto.getId();
-	   String passwd = dto.getPassword();
-	   String Win = "0";
-	   String Lose = "0";
- 
-	   String sql = "INSERT INTO guest(id, password, Win, Lose) VALUES";
+	   String password = dto.getPassword();
+	   String win = dto.getWin();
+	   String lose = dto.getLose();
+	   
+	   String sql = "INSERT INTO guest(id, password, win, lose) VALUES";
  
 	   try 
 	   {
 		   sql += "('" + new String(id.getBytes(), "ISO-8859-1") + "','"  
-				   + new String(passwd.getBytes(), "ISO-8859-1") + "','"
-				   + new String(Win.getBytes(), "ISO-8859-1") + "','"
-				   + new String(Lose.getBytes(), "ISO-8859-1") + "')";
+				   + new String(password.getBytes(), "ISO-8859-1") + "','"
+				   + new String(win.getBytes(), "ISO-8859-1") + "','"
+				   + new String(lose.getBytes(), "ISO-8859-1") + "')";
     
 		   Class.forName("com.mysql.jdbc.Driver");
 		   con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/member", "root", "1234");
